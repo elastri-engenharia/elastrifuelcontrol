be884d60f9e18a46500501ef7c5a212686cd9df0
Gabriel Adriano <gabrieladrianofx@gmail.com>
2023-10-18 15:01:36 -0300

    chore: creating Profile screen

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,5 +1,18 @@
+import { useState } from "react";
 import { TouchableOpacity } from "react-native";
-import { Center, Heading, ScrollView, Text, VStack } from "native-base";
+
+import * as ImagePicker from "expo-image-picker";
+import * as FileSystem from "expo-file-system";
+
+import {
+  Center,
+  Heading,
+  ScrollView,
+  Text,
+  VStack,
+  useToast,
+  Skeleton,
+} from "native-base";
 
 import userPhotoDefault from "@assets/images/userPhotoDefault.png";
 
@@ -11,19 +24,69 @@ import { Input } from "@components/Input";
 const PHOTO_SIZE = 33;
 
 export function Profile() {
+  const [photoIsLoading, setPhotoIsLoading] = useState(false);
+  const [userPhoto, setUserPhoto] = useState(
+    "https://github.com/gabrieladrianofx.png"
+  );
+
+  const toast = useToast();
+
+  async function handleUserPhotoSelect() {
+    setPhotoIsLoading(true);
+    try {
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        aspect: [4, 4],
+        allowsEditing: true,
+      });
+
+      if (photoSelected.canceled) return;
+
+      if (photoSelected.assets[0].uri) {
+        const photoInfo = await FileSystem.getInfoAsync(
+          photoSelected.assets[0].uri
+        );
+
+        if (photoInfo.exists && photoInfo.size / 1024 / 1024 > 5) {
+          return toast.show({
+            title: "Essa imagem é muito grande. Escolha uma até 5MB.",
+            placement: "top",
+            bgColor: "red.500",
+          });
+        }
+
+        setUserPhoto(photoSelected.assets[0].uri);
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setPhotoIsLoading(false);
+    }
+  }
   return (
     <VStack flex={1}>
       <ScreenHeader title="Perfil" onPress={() => {}} />
 
       <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
         <Center mt={6} px={10}>
-          <UserPhoto
-            source={userPhotoDefault}
-            size={PHOTO_SIZE}
-            alt="Foto do usuário"
-          />
+          {photoIsLoading ? (
+            <Skeleton
+              w={PHOTO_SIZE}
+              h={PHOTO_SIZE}
+              rounded="full"
+              startColor="gray.800"
+              endColor="gray.500"
+            />
+          ) : (
+            <UserPhoto
+              source={{ uri: userPhoto }}
+              alt="Foto do usuário"
+              size={PHOTO_SIZE}
+            />
+          )}
 
-          <TouchableOpacity>
+          <TouchableOpacity onPress={handleUserPhotoSelect}>
             <Text color="black" fontWeight="bold" fontSize="md" mt={2} mb={8}>
               Alterar foto
             </Text>
